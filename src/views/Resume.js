4e594d9806fa69c55b85933054ce34a7f13b9b43
Cameron Cobb <cameronjcobb@gmail.com>
2020-04-02 00:29:30 -0700

    resume page is properly setup, just needs info and dynamically add data

--- src/views/Resume.js
+++ src/views/Resume.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles(theme => ({
             top: 0
         },
         "&:after": {
-            content: '""',
+            content: "''",
             display: "table",
             clear: "both"
         },
@@ -33,6 +33,75 @@ const useStyles = makeStyles(theme => ({
                 right: "auto"
             }
         }
+    },
+    timeLineItem: {
+        padding: "1rem",
+        borderBottom: "2px solid tan",
+        position: "relative",
+        margin: "1rem 3rem 1rem 1rem",
+        clear: "both",
+        "&:after": {
+            content: "''",
+            position: "absolute",
+        },
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            right: "-0.625rem",
+            top: "calc(50% - 5px)",
+            borderStyle: "solid",
+            borderColor: "tomato tomato transparent transparent",
+            borderWidth: "0.625rem",
+            transform: "rotate(45deg)"
+        },
+        [theme.breakpoints.up("md")]: {
+            width: "44%",
+            margin: "1rem",
+            "&:nth-of-type(2n)": {
+                float: "right",
+                margin: "1rem",
+                borderColor: "tan"
+            },
+            "&:nth-of-type(2n):before": {
+                right: "auto",
+                left: "-0.625rem",
+                borderColor: "transparent transparent tomato tomato"
+            },
+        }
+    },
+    timeLineYear: {
+        textAlign: "center",
+        maxWidth: "9.375rem",
+        margin: "0 3rem 0 auto",
+        fontSize: "1.8rem",
+        background: "tomato",
+        color: "white",
+        lineHeight: 1,
+        padding: "0.5rem 0 1rem",
+        "&:before": {
+            display: "none"
+        },
+        [theme.breakpoints.up("md")]: {
+            textAlign: "center",
+            margin: "0 auto",
+            "&:nth-of-type(2n)": {
+                float: "none",
+                margin: "0 auto"
+            },
+            "&:nth-of-type(2n):before": {
+                display: "none"
+            }
+        }
+    },
+    heading: {
+        color: "tomato",
+        padding: "3rem 0",
+        textTransform: "uppercase"
+    },
+    subHeading: {
+        color: "white",
+        padding: "0",
+        textTransform: "uppercase"
     }
 }))
 
@@ -43,13 +112,40 @@ const Resume = () => {
         <React.Fragment>
             <NavBar />
             <Box component="header" className={classes.mainContainer}>
-                <Typography variant="h4" align="center">
+                <Typography variant="h4" align="center" className={classes.heading}>
                     Working Exp...
                 </Typography>
                 <Box component="div" className={classes.timeLine}>
-                    <Typography variant="h2">
+                    {/* Good idea to add data to an array and dynamically add this in */}
+                    
+                    <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
                         2013
                     </Typography>
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="left"  className={classes.subHeading}>
+                            Web design*
+                        </Typography>
+                        <Typography variant="body1" align="left" style={{color: "tomato"}}>
+                            Company where worked
+                        </Typography>
+                        <Typography variant="subtitle1" align="left" style={{color: "tan"}}>
+                            Lorem asdfasdf
+                        </Typography>
+                    </Box>
+                    <Typography variant="h2" className={`${classes.timeLineYear} ${classes.timeLineItem}`}>
+                        2014
+                    </Typography>
+                    <Box component="div" className={classes.timeLineItem}>
+                        <Typography variant="h5" align="left"  className={classes.subHeading}>
+                            C++*
+                        </Typography>
+                        <Typography variant="body1" align="left" style={{color: "tomato"}}>
+                            Company where worked again
+                        </Typography>
+                        <Typography variant="subtitle1" align="left" style={{color: "tan"}}>
+                            Lorem asdfasdf
+                        </Typography>
+                    </Box>
                 </Box>
             </Box>
         </React.Fragment>
